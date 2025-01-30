8ffcbe1702831db10e8dc46082e6f4b0e8703750
Kat <siliconfoxx@protonmail.com>
2025-01-29 19:13:14 -0500

    added null option handling

--- client/src/components/Guild/Rankings.tsx
+++ client/src/components/Guild/Rankings.tsx
@@ -1,117 +1,124 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import { Box, Paper } from '@mui/material'
-import ProgressAccordian from '../Leaderboard/ProgressAccordian.tsx';
-import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
-import MuiAccordion, { AccordionProps } from '@mui/material/Accordion';
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import { Box, Paper } from "@mui/material";
+import ProgressAccordian from "../Leaderboard/ProgressAccordian.tsx";
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
 import MuiAccordionSummary, {
   AccordionSummaryProps,
-} from '@mui/material/AccordionSummary';
-import MuiAccordionDetails from '@mui/material/AccordionDetails';
-import { AffixContext } from '../../context/AffixContext';
-import { CircularProgress, Tooltip } from '@mui/material';
-import '../../App.css';
-import { useContext, useEffect, useState } from 'react';
-import { GuildKillRank, RaidRankings } from '../../Models/guildModel';
-import { RaidModel } from '../../Models/raidModel';
-import { prettyNumberFormat } from '../../util/util';
-import ParsedColoredText from '../Generics/ParsedColoredText';
-import { GuildContext } from '../../context/GuildContext';
-import ShieldIcon from '@mui/icons-material/Shield';
-import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh'; //for dps icon
-import Diversity3Icon from '@mui/icons-material/Diversity3'; //for average ilvl
-import AddIcon from '@mui/icons-material/Add';
+} from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import { AffixContext } from "../../context/AffixContext";
+import { CircularProgress, Tooltip } from "@mui/material";
+import "../../App.css";
+import { useContext, useEffect, useState } from "react";
+import { GuildKillRank, RaidRankings } from "../../Models/guildModel";
+import { RaidModel } from "../../Models/raidModel";
+import { prettyNumberFormat } from "../../util/util";
+import ParsedColoredText from "../Generics/ParsedColoredText";
+import { GuildContext } from "../../context/GuildContext";
+import ShieldIcon from "@mui/icons-material/Shield";
+import AutoFixHighIcon from "@mui/icons-material/AutoFixHigh"; //for dps icon
+import Diversity3Icon from "@mui/icons-material/Diversity3"; //for average ilvl
+import AddIcon from "@mui/icons-material/Add";
 
 const Accordion = styled((props: AccordionProps) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
 ))(({ theme }) => ({
   border: `1px solid ${theme.palette.divider}`,
-  '&:not(:last-child)': {
+  "&:not(:last-child)": {
     borderBottom: 0,
   },
-  '&::before': {
-    display: 'none',
+  "&::before": {
+    display: "none",
   },
 }));
 
 const AccordionSummary = styled((props: AccordionSummaryProps) => (
   <MuiAccordionSummary
-    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
     {...props}
   />
 ))(({ theme }) => ({
   backgroundColor:
-    theme.palette.mode === 'dark'
-      ? 'rgba(255, 255, 255, .05)'
-      : 'rgba(0, 0, 0, .03)',
-  flexDirection: 'row-reverse',
-  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
-    transform: 'rotate(90deg)',
+    theme.palette.mode === "dark"
+      ? "rgba(255, 255, 255, .05)"
+      : "rgba(0, 0, 0, .03)",
+  flexDirection: "row-reverse",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
   },
-  '& .MuiAccordionSummary-content': {
+  "& .MuiAccordionSummary-content": {
     marginLeft: theme.spacing(1),
-  }, 
+  },
 }));
 
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
   padding: theme.spacing(2),
-  borderTop: '1px solid rgba(0, 0, 0, .125)',
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
 }));
 
 interface GroupCompModel {
-  tanks: number,
-  healers: number,
-  dps: number
+  tanks: number;
+  healers: number;
+  dps: number;
 }
 
 const Rankings = () => {
   const [expanded, setExpanded] = useState<string | false>(false);
   const [rankings, setRankings] = useState<GuildKillRank | null>(null);
   const [bosses, setBosses] = useState<RaidModel[] | null>(null);
-  const [avgIlvl, setAvgIlvl] = useState<number | null>(null);
-  const [groupComp, setGroupComp] = useState<GroupCompModel>({tanks: 0, healers: 0, dps: 0});
+  const [avgIlvl, setAvgIlvl] = useState<number | null>(0);
+  const [groupComp, setGroupComp] = useState<GroupCompModel>({
+    tanks: 0,
+    healers: 0,
+    dps: 0,
+  });
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
-  const handleChange = (panel: string,  bossSlug: string) => (e: React.SyntheticEvent, newExpanded: boolean) => {
-    setExpanded(newExpanded ? panel : false);
-    if (newExpanded) {
-      determineGroupComp(bossSlug);
-    }
-  };
+  const handleChange =
+    (panel: string, bossSlug: string) =>
+    (e: React.SyntheticEvent, newExpanded: boolean) => {
+      setExpanded(newExpanded ? panel : false);
+      //making sure that this is not null
+      if (newExpanded && bosses) {
+        determineGroupComp(bossSlug);
+      }
+    };
 
   useEffect(() => {
-    fetch('http://localhost:8080/killRank')
-      .then(resp => {
+    fetch("http://localhost:8080/killRank")
+      .then((resp) => {
         if (!resp.ok) {
-          throw new Error('Network response no bueno');
+          throw new Error("Network response no bueno");
         }
         return resp.json();
       })
       .then((data: GuildKillRank) => {
-        setRankings(data.raid_rankings['nerubar-palace']); //only available for latest raid
+        setRankings(data.raid_rankings["nerubar-palace"]); //only available for latest raid
         setIsLoading(false);
       })
-      .catch(err => {
+      .catch((err) => {
         setIsLoading(false);
         setError(err.message);
       });
   }, []);
 
-//fetch for bossnames. etc...
+  //fetch for bossnames. etc...
   useEffect(() => {
-    fetch('http://localhost:8080/staticRaidData')
-      .then(resp => {
+    fetch("http://localhost:8080/staticRaidData")
+      .then((resp) => {
         if (!resp.ok) {
-          throw new Error('Network response not good');
+          throw new Error("Network response not good");
         }
         return resp.json();
       })
-      .then(data => {
+      .then((data) => {
         setBosses(data);
         setIsLoading(false);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
         setIsLoading(false);
         setError(err);
@@ -120,29 +127,40 @@ const Rankings = () => {
 
   const determineGroupComp = (bossSlug: string) => {
     fetch(`http://localhost:8080/detailedEncounter?bossSlug=${bossSlug}`)
-      .then(resp => {
+      .then((resp) => {
         if (!resp.ok) {
-          throw new Error('Network response not good');
+          throw new Error("Network response not good");
         }
         return resp.json();
       })
-      .then(data => {
+      .then((data) => {
         console.log(data);
-        let ilvlArr = []; 
+        if (!Array.isArray(data)) {
+          console.error("expected an array but recieved: ", data);
+          return;
+        }
+        let ilvlArr = [];
         let initComp: GroupCompModel = { tanks: 0, healers: 0, dps: 0 };
 
-        data.forEach(character => {
+        data.forEach((character) => {
           ilvlArr.push(character.itemLevelEquipped);
-          
+
           //incrementing role
-          if (character.specRole === 'tank') initComp.tanks+= 1; 
-          if (character.specRole === 'dps') initComp.dps+= 1;
-          if (character.specRole === 'healer') initComp.healers+= 1;
+          if (character.specRole === "tank") initComp.tanks += 1;
+          if (character.specRole === "dps") initComp.dps += 1;
+          if (character.specRole === "healer") initComp.healers += 1;
         });
         setGroupComp(initComp);
-        setAvgIlvl(Math.floor(ilvlArr.length > 0 ? ilvlArr.reduce((sum, currentValue) => sum + currentValue, 0) / ilvlArr.length : 0));
+        setAvgIlvl(
+          Math.floor(
+            ilvlArr.length > 0
+              ? ilvlArr.reduce((sum, currentValue) => sum + currentValue, 0) /
+                  ilvlArr.length
+              : 0,
+          ),
+        );
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   };
@@ -158,65 +176,116 @@ const Rankings = () => {
   return (
     <div>
       <h3>Boss Kill Rankings</h3>
-      <strong>World: <ParsedColoredText number={rankings.mythic.world}/> &nbsp; Region : <ParsedColoredText number={rankings.mythic.region}/> &nbsp; Realm: <ParsedColoredText number={rankings.mythic.realm}/></strong>
+      <strong>
+        World: <ParsedColoredText number={rankings.mythic.world} /> &nbsp;
+        Region : <ParsedColoredText number={rankings.mythic.region} /> &nbsp;
+        Realm: <ParsedColoredText number={rankings.mythic.realm} />
+      </strong>
       <br />
-      <Box sx={{ width: '100%' }}>
-        <Paper sx={{ width: '100%', mb: 2, pt: '1rem' }}>
+      <Box sx={{ width: "100%" }}>
+        <Paper sx={{ width: "100%", mb: 2, pt: "1rem" }}>
           {isLoading ? (
             <CircularProgress />
           ) : (
-            bosses && bosses.map((boss, idx) => (
-              <Accordion key={idx} expanded={expanded === `panel${idx}`} onChange={handleChange(`panel${idx}`, boss.slug)}>
-                <AccordionSummary aria-controls={`panel${idx}d-content`} id={`panel${idx}d-header`}>
-                  {boss.name} 
+            bosses &&
+            bosses.map((boss, idx) => (
+              <Accordion
+                key={idx}
+                expanded={expanded === `panel${idx}`}
+                onChange={handleChange(`panel${idx}`, boss.slug)}
+              >
+                <AccordionSummary
+                  aria-controls={`panel${idx}d-content`}
+                  id={`panel${idx}d-header`}
+                >
+                  {boss.name}
                 </AccordionSummary>
                 <AccordionDetails>
-                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
-                    <Tooltip 
-                      title='Average item level of the raid during boss kill' 
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      width: "100%",
+                    }}
+                  >
+                    <Tooltip
+                      title="Average item level of the raid during boss kill"
                       followCursor
                       componentsProps={{
                         tooltip: {
-                          sx: { fontSize: '1rem'}
-                        }
-                      }}>
-                      <span style={{ fontSize: '4rem', display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+                          sx: { fontSize: "1rem" },
+                        },
+                      }}
+                    >
+                      <span
+                        style={{
+                          fontSize: "4rem",
+                          display: "flex",
+                          alignItems: "center",
+                          gap: "0.5rem",
+                        }}
+                      >
                         <Diversity3Icon fontSize="inherit" /> {avgIlvl}
-                     </span>
+                      </span>
                     </Tooltip>
-                    <Tooltip 
-                      title='Number of tanks present during boss kill' 
+                    <Tooltip
+                      title="Number of tanks present during boss kill"
                       followCursor
                       componentsProps={{
                         tooltip: {
-                          sx: { fontSize: '1rem'}
-                        }
-                      }}>
-                      <span style={{ fontSize: '4rem', display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+                          sx: { fontSize: "1rem" },
+                        },
+                      }}
+                    >
+                      <span
+                        style={{
+                          fontSize: "4rem",
+                          display: "flex",
+                          alignItems: "center",
+                          gap: "0.5rem",
+                        }}
+                      >
                         <ShieldIcon fontSize="inherit" /> {groupComp.tanks}
                       </span>
                     </Tooltip>
-                    <Tooltip 
-                      title='Number of healers present during boss kill' 
+                    <Tooltip
+                      title="Number of healers present during boss kill"
                       followCursor
                       componentsProps={{
                         tooltip: {
-                          sx: { fontSize: '1rem'}
-                        }
-                      }}>
-                      <span style={{ fontSize: '4rem', display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+                          sx: { fontSize: "1rem" },
+                        },
+                      }}
+                    >
+                      <span
+                        style={{
+                          fontSize: "4rem",
+                          display: "flex",
+                          alignItems: "center",
+                          gap: "0.5rem",
+                        }}
+                      >
                         <AddIcon fontSize="inherit" /> {groupComp.healers}
                       </span>
                     </Tooltip>
-                    <Tooltip 
-                      title='Number of dps present during boss kill' 
+                    <Tooltip
+                      title="Number of dps present during boss kill"
                       followCursor
                       componentsProps={{
                         tooltip: {
-                          sx: { fontSize: '1rem'}
-                        }
-                      }}>
-                      <span style={{ fontSize: '4rem', display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+                          sx: { fontSize: "1rem" },
+                        },
+                      }}
+                    >
+                      <span
+                        style={{
+                          fontSize: "4rem",
+                          display: "flex",
+                          alignItems: "center",
+                          gap: "0.5rem",
+                        }}
+                      >
                         <AutoFixHighIcon fontSize="inherit" /> {groupComp.dps}
                       </span>
                     </Tooltip>
