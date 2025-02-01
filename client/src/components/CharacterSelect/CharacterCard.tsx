b9bdfab0343ce30050e9f1417f391681e1833851
Kat <siliconfoxx@protonmail.com>
2025-01-31 19:30:58 -0500

    added conditional render of iframe if character is set favorite

--- client/src/components/CharacterSelect/CharacterCard.tsx
+++ client/src/components/CharacterSelect/CharacterCard.tsx
@@ -6,7 +6,7 @@ import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import { useState } from "react";
 import { BorderColor } from "@mui/icons-material";
 import { Grid } from "@mui/material";
-import Heart from "../Generics/Heart";
+import { styled } from "@mui/material/styles";
 
 interface CharacterCardProps {
   charId: number;
@@ -14,9 +14,31 @@ interface CharacterCardProps {
   favorite: boolean;
 }
 
+//stlyed component for iframe to have slide in and out animation based on favorite
+const IFrameComponent = styled("div")<{ isVisible: boolean }>(
+  //adding the isVisible prop to component
+  ({ isVisible }) => ({
+    width: "100%", //container takes up 100% of parent
+    height: "18vh", //based on viewport height
+    overflow: "hidden",
+    position: "relative",
+    transform: isVisible ? "translateX(0)" : "translateX(-100%)", //if visible (favorite is set to true) is true, don't do anything. else do slide out animation
+    transition: "transform 0.5s ease-out", // property name | duration | easing function
+    "& iframe": {
+      //elements within IframeComponent
+      width: "100%",
+      height: "100%",
+      border: "none",
+    },
+  }),
+);
+
 const CharacterCard = (props: CharacterCardProps) => {
   const { charId, name, favorite } = props;
   const [isFavorite, setIsFavorite] = useState(favorite); //false for now, default value will be based on user context
+  const [isVisible, setIsVisible] = useState(favorite);
+  const widgetUrl =
+    "https://raider.io/characters/us/stormrage/Foxxbozo?embed=1&embedmode=summary&embedName=1&classcolors=1&characterBackground=1&itemlevelcolors=1&showtime=10&chromargb=transparent";
 
   const cardNavStyles = {
     color: "inherit",
@@ -31,7 +53,7 @@ const CharacterCard = (props: CharacterCardProps) => {
     fontFamily: "Poppins",
   };
 
-  const handleSetFavorite = (e: React.MouseEvent<HTMLDivElement>) => {
+  const handleSetFavorite = (e: React.MouseEvent<SVGSVGElement>) => {
     //when user presses heart button, set it as favorite to backend
     setIsFavorite(!isFavorite);
     //update userContext in db
@@ -43,20 +65,41 @@ const CharacterCard = (props: CharacterCardProps) => {
       <Card sx={{ minWidth: 220, paddingBottom: "1rem", paddingTop: "1rem" }}>
         <Grid container>
           <Grid item xs={8}>
-            <CardContent
-              component={NavLink}
-              to={`/${name}-${charId}`}
-              sx={cardNavStyles}
-            >
-              {name}
-            </CardContent>
+            {isFavorite ? (
+              <CardContent
+                component={NavLink}
+                to={`/${name}-${charId}`}
+                sx={cardNavStyles}
+              >
+                <IFrameComponent isVisible={isFavorite}>
+                  <iframe src={widgetUrl} />
+                </IFrameComponent>
+              </CardContent>
+            ) : (
+              <div>frick</div>
+            )}
           </Grid>
           <Grid>
             <CardContent>
               {isFavorite ? (
-                <Heart onClick={handleSetFavorite} size={40} />
+                <FavoriteIcon
+                  onClick={handleSetFavorite}
+                  sx={{
+                    color: "#FF91AF",
+                    "&:hover": {
+                      color: "#FF69B4",
+                    },
+                  }}
+                />
               ) : (
-                <Heart isBreaking onClick={handleSetFavorite} size={40} />
+                <FavoriteBorderIcon
+                  onClick={handleSetFavorite}
+                  sx={{
+                    "&:hover": {
+                      color: "#FF69B4",
+                    },
+                  }}
+                />
               )}
             </CardContent>
           </Grid>
--- client/src/components/Generics/Heart.tsx
+++ client/src/components/Generics/Heart.tsx
@@ -1,28 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { styled, keyframes } from "@mui/system";
 
 // Define the heartbeat animation
 const heartbeat = keyframes`
   0% {
-    transform: scale(1) rotate(-45deg);
+    transform: scale(1);
   }
   25% {
-    transform: scale(1.25) rotate(-45deg);
+    transform: scale(1.25);
   }
   45% {
-    transform: scale(1.5) rotate(-45deg);
-  }
-`;
-
-// Define the break animation
-const breakAnimation = keyframes`
-  0% {
-    transform: translateY(0) rotate(-45deg);
-    opacity: 1;
-  }
-  100% {
-    transform: translateY(50px) rotate(-45deg);
-    opacity: 0;
+    transform: scale(1.5);
   }
 `;
 
@@ -31,12 +19,12 @@ const Heart = styled("div")<{ isBreaking?: boolean; size?: number }>`
   height: ${({ size }) => size || 100}px;
   width: ${({ size }) => size || 100}px;
   background-color: #ff91af;
-  transform: rotate(-45deg);
+  rotate: 45deg;
   position: relative;
   cursor: pointer;
 
   &:hover {
-    animation: ${({ isBreaking }) => (isBreaking ? "none" : heartbeat)} 1s;
+    animation: ${heartbeat} 1s;
   }
 
   &::before,
@@ -47,19 +35,37 @@ const Heart = styled("div")<{ isBreaking?: boolean; size?: number }>`
     background-color: #ff91af;
     border-radius: 50%;
     position: absolute;
-    animation: ${({ isBreaking }) => (isBreaking ? breakAnimation : "none")} 1s
-      forwards;
   }
 
   &::before {
-    top: -${({ size }) => (size ? size / 2 : 50)}px;
+    top: -${({ size }) => (size ? size / 2 : 15)}px;
     left: 0;
   }
 
   &::after {
-    left: -${({ size }) => (size ? size / 2 : 50)}px;
+    left: -${({ size }) => (size ? size / 2 : 15)}px;
     top: 0;
   }
 `;
 
-export default Heart;
+// Heart component with click handler
+const HeartComponent: React.FC = () => {
+  const [isBeating, setIsBeating] = useState(false);
+
+  const handleClick = () => {
+    setIsBeating(true);
+    setTimeout(() => setIsBeating(false), 1000); // Reset after animation ends
+  };
+
+  return (
+    <Heart
+      size={100}
+      onClick={handleClick}
+      style={{
+        animation: isBeating ? `${heartbeat} 1s` : "none",
+      }}
+    />
+  );
+};
+
+export default HeartComponent;
