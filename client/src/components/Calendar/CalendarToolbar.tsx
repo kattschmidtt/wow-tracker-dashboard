54b817fee9f49ff21e4034336c1b8eafda73ef82
Kat <siliconfoxx@protonmail.com>
2025-02-02 02:06:25 -0500

    adding basic functionality to addevent and making modal modular

--- client/src/components/Calendar/CalendarToolbar.tsx
+++ client/src/components/Calendar/CalendarToolbar.tsx
@@ -1,34 +1,79 @@
-import { Button, ButtonGroup, Fab, Tooltip } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import { ToolbarProps } from 'react-big-calendar';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-
+import { useState } from "react";
+import InputModal from "../Generics/InputModal";
+import { Button, ButtonGroup, Fab, Tooltip } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import { ToolbarProps } from "react-big-calendar";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 function CalendarToolbar(props: ToolbarProps) {
   const { label, onNavigate, onView } = props; //from react-big-calendar
+  const [addEventModalOpen, setAddEventModalOpen] = useState(false);
+
+  //to bring up the add custom event modal
+  const handleClickOpen = () => {
+    setAddEventModalOpen(true);
+  };
+  const handleClickClose = () => {
+    setAddEventModalOpen(false);
+  };
 
   return (
     <>
-      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', marginBottom: '1rem'}}>
-        <Button onClick={() => onNavigate('PREV')}><ArrowBackIosIcon /></Button>
-        <h1 style={{ margin: '0 1rem' }}>{label}</h1>
-        <Button onClick={() => onNavigate('NEXT')}><ArrowForwardIosIcon /></Button>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginBottom: "1rem",
+        }}
+      >
+        <Button onClick={() => onNavigate("PREV")}>
+          <ArrowBackIosIcon />
+        </Button>
+        <h1 style={{ margin: "0 1rem" }}>{label}</h1>
+        <Button onClick={() => onNavigate("NEXT")}>
+          <ArrowForwardIosIcon />
+        </Button>
       </div>
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '0.5rem' }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "0.5rem",
+        }}
+      >
         <div>
           <ButtonGroup variant="text">
-            <Button variant="outlined" onClick={() => onView('month')}>Month</Button>
-            <Button variant="outlined" onClick={() => onView('week')}>Week</Button>
-            <Button variant="outlined" onClick={() => onView('day')}>Day</Button>
+            <Button variant="outlined" onClick={() => onView("month")}>
+              Month
+            </Button>
+            <Button variant="outlined" onClick={() => onView("week")}>
+              Week
+            </Button>
+            <Button variant="outlined" onClick={() => onView("day")}>
+              Day
+            </Button>
           </ButtonGroup>
         </div>
 
         <div>
           <Tooltip title="Add custom event" placement="left">
-            <Fab onClick={() => alert('clicked :)')} size="small" color="primary" aria-label="add-event">
+            <Fab
+              onClick={handleClickOpen}
+              size="small"
+              color="primary"
+              aria-label="add-event"
+            >
               <AddIcon />
             </Fab>
+            <InputModal
+              open={addEventModalOpen}
+              close={handleClickClose}
+              title={"Add an event"}
+              eventType={"Add"}
+            />
           </Tooltip>
         </div>
       </div>
@@ -37,3 +82,4 @@ function CalendarToolbar(props: ToolbarProps) {
 }
 
 export default CalendarToolbar;
+
--- client/src/components/Generics/InputModal.tsx
+++ client/src/components/Generics/InputModal.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { forwardRef, ReactElement } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Zoom } from "@mui/material";
+import { TransitionProps } from "@mui/material/transitions";
 
-const InputModal = () => {
+const Transition = forwardRef(function Transition(
+  props: TransitionProps & {
+    children: ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Zoom ref={ref} {...props} />;
+});
+
+interface InputModalProps {
+  open: boolean;
+  close: () => void;
+  title: string;
+  eventType: string;
+}
+
+const InputModal = ({ open, close, title, eventType }: InputModalProps) => {
+  const handleAddEvent = () => {
+    alert("you have added the event yippie!");
+  };
+
   return (
-    <div>
-      input modal will pop up here 
-    </div>
+    <Dialog open={open} onClose={close} TransitionComponent={Transition}>
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          this would be the super informative form
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleAddEvent}>{eventType}</Button>
+        <Button onClick={close}>Cancel</Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
 export default InputModal;
+
